eb78e2090341e6d6dd36b764e55a820474c45994
hk414 <hui.koh-2@student.manchester.ac.uk>
2024-03-08 10:08:55 +0000

    index.php shortcut logo

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>3Chain</title>
+    <link rel="shortcut icon" type="image/png" href="Image_used\landing_page\logo.png" />
     <link rel="stylesheet" href="css/reset.css" />
     <link rel="stylesheet" href="css/normalize.css" />
     <link rel="stylesheet" href="css/styles.css" />
