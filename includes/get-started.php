d520309de4ca376869c542ea1737d7401c5ae210
hk414 <hui.koh-2@student.manchester.ac.uk>
2024-03-14 11:32:57 +0000

    get started redirect to login

--- includes/get-started.php
+++ includes/get-started.php
@@ -8,7 +8,7 @@
             <div class="back">
               <div>
                 <p class="text text--smallcloser">Begin your journey into the world of 3Chain by creating your personalized account or logging in to unlock exclusive access to our immersive blockchain learning platform.</p>
-                <button class="value-proposition__button btn btn--accent">Start Learning</button>
+                <button class="value-proposition__button btn btn--accent" onclick="redirectToLoginPage()">Start Learning</button>
               </div>
             </div>
           </div>
@@ -19,7 +19,7 @@
             <div class="back">
               <div>
                 <p class="text text--smallcloser ">Navigate through our interactive modules and unlock the secrets of decentralized networks, smart contracts, and token economies. Engage with cutting-edge content designed to expand your understanding and ignite your curiosity.</p>
-                <button class="value-proposition__button btn btn--accent">Start Learning</button>
+                <button class="value-proposition__button btn btn--accent" onclick="redirectToLoginPage()">Start Learning</button>
               </div>
             </div>
           </div>
@@ -30,7 +30,7 @@
             <div class="back">
               <div>
                 <p class="text text--smallcloser">Validate your mastery of Web3 and blockchain concepts by completing our comprehensive learning modules. Achieve proficiency and unlock certification badges that showcase your expertise to the world. Empower yourself with credentials recognized across industries and join the ranks of blockchain innovators.</p>
-                <button class="value-proposition__button btn btn--accent">Start Learning</button>
+                <button class="value-proposition__button btn btn--accent" onclick="redirectToLoginPage()">Start Learning</button>
               </div>
             </div>
           </div>
