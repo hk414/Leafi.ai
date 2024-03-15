f931c79b0050c4ea0ac07c42b32da6bee518dd40
hk414 <hui.koh-2@student.manchester.ac.uk>
2024-03-15 08:15:45 +0000

    add c3

---
+++ user/challenges/c3.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>3Chain.ai</title>
+  <link rel="shortcut icon" type="image/png" href="../assets/images/logos/logo.png" />
+  <link rel="stylesheet" href="../assets/css/styles.min.css" />
+  <link rel="stylesheet" href="../assets/css/chatbot.css" />
+  <link rel="stylesheet" href="../assets/css/styles2.css" />
+  <link rel="stylesheet" href="assets/hash.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+</head>
+
+<body>
+  <!--  Body Wrapper -->
+  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+    data-sidebar-position="fixed" data-header-position="fixed">
+
+    <!-- Sidebar Start -->
+    <?php include 'includes/sidebar.html';?>
+    <!--  Sidebar End -->
+
+    <!--  Main wrapper -->
+    <div class="body-wrapper">
+
+      <!--  Header Start -->
+      <?php include 'includes/header.html';?>
+      <!--  Header End -->
+    </div>
+
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+      <!-- Footer -->
+      <?php include '../includes/footer.html';?>
+
+      <!-- Chatbot -->
+      <?php include '../includes/chatbot.html';?>
+
+    </div>
+  </div>
+  <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
+  <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/js/sidebarmenu.js"></script>
+  <script src="../assets/js/app.min.js"></script>
+  <script src="../assets/libs/apexcharts/dist/apexcharts.min.js"></script>
+  <script src="../assets/libs/simplebar/dist/simplebar.js"></script>
+  <script src="../assets/js/dashboard.js"></script>
+  <script src="../assets/js/chatbot.js"></script>
+  
+</body>
+
+</html>
